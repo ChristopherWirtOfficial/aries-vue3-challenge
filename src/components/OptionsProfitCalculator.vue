dd587751e46027ccd5c389de8875bd882adf92ba
Christopher Wirt <chrisazy@gmail.com>
2024-06-19 19:21:43 -0500

    Clean up and comments on how to complete the graph logic

--- src/components/OptionsProfitCalculator.vue
+++ src/components/OptionsProfitCalculator.vue
@@ -8,15 +8,14 @@ const chartOptions = {
     plugins: {
             annotation: {
                 annotations: {
-                    lineAtZero: {
-                        drawTime: 'beforeDatasetsDraw',
+                    breakevenLine: {
+                        drawTime: 'beforeDatasetsDraw', // Ensures the data draws over the break-even line
                         type: 'line',
                         yMin: 0,
                         yMax: 0,
                         borderColor: 'white',
                         borderWidth: 2,
                         borderDash: [8, 3], // Optional: for a dashed line,
-                        
                     }
                 }
             }
@@ -32,10 +31,7 @@ const options = ref([
 
 const selectedOptions = computed(() => options.value.filter(option => option.enabled));
 
-const N_STEPS = 10;
-// For every selected option, we compute several potential expiry price steps
-//   from -2.5*premium to 2.5*premium on either side of the strike price
-//   and calculate the profit/loss at each step
+const N_STEPS = 20;
 
 type OptionsChartData = {
     id: number;
@@ -51,7 +47,7 @@ const potentialExpiryPriceSteps = computed<OptionsChartData[]>(() => {
         const premium = option.long_short === 'long' ? option.ask : option.bid;
 
         // From -2.5*premium to 2.5*premium, which we'll then calculate potentialPrices from based on strike_price + potentialPriceDeviation
-        const potentialPriceDeviations = Array.from({ length: N_STEPS }, (_, i) => -2.5 + (i / (N_STEPS)) * 5);
+        const potentialPriceDeviations = Array.from({ length: N_STEPS }, (_, i) => -2.5 + (i / (N_STEPS )) * 10);
         const potentialPrices = potentialPriceDeviations
             .map(deviation => option.strike_price + deviation * premium)
             .map(price => Math.round(price * 100) / 100);
@@ -72,13 +68,29 @@ const potentialExpiryPriceSteps = computed<OptionsChartData[]>(() => {
 });
 
 const chartData = computed(() => ({
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: potentialExpiryPriceSteps.value.map(option => ({
         label: `Option ${option.id}`,
         data: option.potentialPrices.map((price, i) => ({ x: price, y: option.potentialProfitOrLoss[i] })),
         backgroundColor: options.value[option.id].color,
     })),
 }));
+
+
+/*
+    TODO: Changes to make
+    - Go back to a (dyanmic) fixed set of prices. Based on the min/max price, have N steps between the global min/max price
+    - Specifically insert (unless present) the break-even price of each option into our set of X values
+    - ONLY THEN after constructing the X values do we calculate the profits exercising any given option at every X value
+    - Then we add the profit (or loss, if negative) at each X value to get our actual Y value for total proift/loss at each price
+
+    All of this is because these options are for the same instrument. You want to maximize the profit over all of the selected options
+       as if they were all going to be executed by the same person, not individually. 
+
+    We'll then be able to find which options you should select as a function of the expected exercise price
+
+    TODO: Stretch Goals
+    - Create small cards for each option, and place them vertically on the side of the graph. Both display and selection controls.
+*/
 
 console.log(potentialExpiryPriceSteps.value);
 </script>
@@ -95,14 +107,6 @@ console.log(potentialExpiryPriceSteps.value);
         <input type="checkbox" v-model="option.enabled" />
         <label>Option {{ option.id }}</label>
         </div>
-    </div>
-    <div>
-        <h2>Selected Options</h2>
-        <ul>
-            <li v-for="option in selectedOptions" :key="option.id">
-                Option {{ option.id }}
-            </li>
-        </ul>
     </div>
 </template>
 
